ff12eeee0814a1a5bc6559ffa4414b1eb1ff5cdb
Soundryacedcoss <soundryarani@cedcoss.com>
2022-11-24 13:15:13 +0530

    done

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-console.log('hello redux');
 const redux=require('redux')
 const BUY_CAKE='BUY_CAKE';
 // Here i am creating store for holding the states value
 const createStore=redux.createStore
-// creatting action
-
-
+// creating ACTION
 function buycake(){
     return{
         type:BUY_CAKE,
         info:"Buy first cake "
     }
 }
-
 // Here is my store which will contain states of my app
 const initialState={
     noOfCakes:10
 }
-// Now we will crete reducer for perform action and update state according to action.
+// Now we will create reducer for perform action and update state according to action.
 const reducer=(state=initialState,action)=>{
   switch(action.type){
     case BUY_CAKE:return{
@@ -27,19 +23,18 @@ const reducer=(state=initialState,action)=>{
     default: return state
   }
 }
-// console.log('stes',state.g);
-// Here i am using the cretestore value
+// Here i am using cretestore 
 const store=createStore(reducer)
 console.log('intialvalue',store.getState());
 
-// i will add subscribe method to give permission to change the state value
+// i am adding subscribe method to give permission to change the state value
 const unsubscribe1=store.subscribe(()=>console.log("update",store.getState()))
-store.dispatch(buycake())
-store.dispatch(buycake())
-
-for (let i = 0; i < 8; i++) {
-  store.dispatch(buycake())
+for (let i = 0; i < 10; i++) {
+  var ot1= store.dispatch(buycake())
+  if(store.getState().noOfCakes===0){
+    console.log('Out Of Stock');
+    break;
+  }
 }
-console.log('Out of stock');
 unsubscribe1()
 store.dispatch(buycake())
